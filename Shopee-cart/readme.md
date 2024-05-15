0dbaceb12a224e790e57e75e3843bdf0c2df4c90
Lucas-Sasaki <lucas_sasaki1@hotmail.com>
2024-05-15 12:10:44 -0300

    Commit inicial do projeto Shopee-cart

---
+++ Shopee-cart/readme.md
@@ -0,0 +1,9 @@
+## Objetivo
+
+Criar um carrinho de compras baseado no carrinho de compras da shopee, aonde o carrinho armazena itens e faça o cálculo de bub-itens automaticamente
+
+// dominio da alicação: carrinho de compras
+
+// as entidade representadas
+// -> carrinho
+// -> itens
---
+++ Shopee-cart/src/services/cart.js
@@ -0,0 +1,44 @@
+async function addItem(userCart, item) {
+    userCart.push(item)
+}
+
+async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item) => item.name === name)
+
+    if(index !== -1) {
+        userCart.splice(index, 1)
+    }
+}
+
+async function removeItem(userCart, item) {
+    const indexFound = userCart.findIndex((p) => p.name === item.name)
+    
+    if(indexFound === -1) {
+        console.log("item não encontrado")
+        return
+    }
+
+    if(userCart[indexFound].quantity > 1) {
+        userCart[indexFound].quantity -= 1
+        return
+    }
+
+    if(userCart[indexFound].quantity === 1) {
+        userCart.splice(indexFound, 1)
+        return
+    }
+}
+
+async function calculateTotal(userCart) {
+    const result = userCart.reduce((total, item)=> total + item.subtotal(), 0)
+    console.log(result)
+}
+
+async function displayCart(userCart) {
+    console.log("Lista carrinho da Shopee: ")
+    userCart.forEach((item, index) => {
+        console.log(`${index + 1}. ${item.name} - ${item.price} | ${item.quantity}x | Subtotal = ${item.subtotal()}`)
+    });
+}
+
+export {addItem, deleteItem, removeItem, calculateTotal, displayCart}
